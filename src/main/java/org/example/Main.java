0faa0ee545b325f555d5a5088f42cd58d2fb0b5f
Phantom-Intruder <mewa.bandara@gmail.com>
2023-03-22 15:02:30 +0530

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Path cloneDirectory = Path.of("/home/user/stuff/kubernetes-config/kubernetes-config/src/main/resources/spring-petclinic");
+            if (!Files.exists(cloneDirectory)) {
+                String repoUrl = "git@github.com:spring-projects/spring-petclinic.git";
+                Git.gitClone(cloneDirectory, repoUrl);
+            }
+            ProcessBuilder builder = new ProcessBuilder("./mvnw");
+            builder.directory(new File(cloneDirectory.toString()));
+            Process pro = builder.start();
+            System.out.println("Starting mvn install");
+            pro.waitFor();
+            System.out.println("Finished running mvn");
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
