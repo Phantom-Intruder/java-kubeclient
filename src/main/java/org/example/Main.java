f286eacbede1f0baf87259cfa0532b0170dad0cd
Phantom-Intruder <mewa.bandara@gmail.com>
2023-03-25 14:39:58 +0530

    Added configs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,18 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
+        pullAndBuildSampleApplication();
+        
+    }
+
+    private static void pullAndBuildSampleApplication() {
         try {
             Path cloneDirectory = Path.of("/home/user/stuff/kubernetes-config/kubernetes-config/src/main/resources/spring-petclinic");
             if (!Files.exists(cloneDirectory)) {
                 String repoUrl = "git@github.com:spring-projects/spring-petclinic.git";
                 Git.gitClone(cloneDirectory, repoUrl);
             }
-            ProcessBuilder builder = new ProcessBuilder("./mvnw");
+            ProcessBuilder builder = new ProcessBuilder("./mvnw package");
             builder.directory(new File(cloneDirectory.toString()));
             Process pro = builder.start();
             System.out.println("Starting mvn install");
